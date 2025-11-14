7edd6b340aafae9741de31414e1b91164a5a0adc
whatsername07 <alexwanklyn@gmail.com>
2025-11-14 12:17:13 +0000

    Attempted fix for 200 response

--- app/main.py
+++ app/main.py
@@ -6,9 +6,11 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept[0].sendall(b"HTTP/1.1 200 OK /r/n/r/n")
+    server_socket.listen(0)
+    client_socket, client_address = server_socket.accept()
+    client_socket.sendall(b"HTTP/1.1 200 OK /r/n/r/n")
+    client_socket.close()
     server_socket.accept() # wait for client
-
 
 if __name__ == "__main__":
     main()
