8cbb93b63928438e84de5ce2e4d03f55ef3e2263
whatsername07 <alexwanklyn@gmail.com>
2025-11-14 12:18:39 +0000

    Edited forward slash top backslash

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.listen(0)
     client_socket, client_address = server_socket.accept()
-    client_socket.sendall(b"HTTP/1.1 200 OK /r/n/r/n")
+    client_socket.sendall(b"HTTP/1.1 200 OK \r\n\r\n")
     client_socket.close()
     server_socket.accept() # wait for client
 
